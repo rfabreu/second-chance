6062c3fce885ca7762697cead9492fa6b02abfcb
ThisOneGuy <tahir.arslan@gmail.com>
2022-10-10 21:45:03 -0400

    push code

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 import LoginItem from '../components/LoginItem';
 
 function Login(props) {
-  return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
-      <LoginItem />
-      <h2>Login</h2>
-    </div>
-  );
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', padding: '5px'}}>
+            <Link to="/signup">← Go to Signup</Link>
+            <h2>Login</h2>
+            <LoginItem />
+        </div>
+    );
 }
 
 export default Login;
